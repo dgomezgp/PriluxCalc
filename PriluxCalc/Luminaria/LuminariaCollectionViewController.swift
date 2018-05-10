719750024166b8d683e6f31ccc08b411429b252b
Gato Gato <davidgomez@macbook-air-de-david.local>
2018-05-10 18:15:14 +0200

    JSON cargado

--- PriluxCalc/Luminaria/LuminariaCollectionViewController.swift
+++ PriluxCalc/Luminaria/LuminariaCollectionViewController.swift
@@ -20,12 +20,46 @@ class LuminariaCollectionViewController: UICollectionViewController {
                                   Luminaria(nombre: "Avatar", lumenes: 600, apertura: 120)]
     
     var indexPathCollection = 0
+    //JSON
+    var luminariasDic = [[String:String]]()
+    var urlAPIString = "http:www.grupoprilux.com/priluxcalc/test.php"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.delegate = self
+        
+        
+        //JSON
+        if let url = URL (string: urlAPIString) {
+            
+            if let data = try? Data(contentsOf: url) {
+                let json = try? JSON(data:data)
+                print(json ?? "No se han encontrado datos")
+                //print(json!["NOMBRE"])
+                print ("----------------------------------------")
+                self.parse(json: json!)
+                /*
+                 
+                 DispatchQueue.global(qos: .userInitiated).async {
+                 if let url = URL (string: urlAPIString) {
+                 
+                 if let data = try? Data(contentsOf: url) {
+                 let json = try? JSON(data:data)
+                 print(json ?? "No se han encontrado datos")
+                 print(json!["data"]["first_name"])
+                 print ("----------------------------------------")
+                 self.parse(json: json!)
+                 
+                 }
+                 */
+                
+                
+            }
+        }
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -42,6 +76,46 @@ class LuminariaCollectionViewController: UICollectionViewController {
      }
      */
     
+    
+    // MARK: TRATAMIENTO CON JSON
+    
+    
+    func parse (json:JSON) {
+        //esto es para borrar el array para poder hacer otras busquedas
+        luminariasDic.removeAll()
+        for resultado in json[].arrayValue {
+            //print ("resultado es \(resultado)")
+            //print ("first name es \(resultado["first_name"].stringValue)")
+            
+            
+            //Obetenemos los valores del json
+            let nombre = resultado["NOMBRE"].stringValue
+            let lumenes = resultado["LUMENES"].stringValue
+            let apertura = resultado["APERTURA"].stringValue
+            let foto = resultado["FOTO"].stringValue
+            //print(resultado["NOMBRE"])
+            //print(resultado["FOTO"])
+            
+            // Creamos un diccionario con todos los valores recogidos
+            
+            let dic = ["NOMBRE":nombre,"LUMENES":lumenes,"APERTURA":apertura,"FOTO":foto] as [String : String]
+            
+            
+            //Añadimos este diccionario a nustro array de diccionarios (petitions)
+            // append es para añadir en la utlima posicion de un array
+            luminariasDic.append(dic)
+            
+        }
+        DispatchQueue.main.async {
+            self.collectionView?.reloadData()
+        }
+        
+    }
+    
+    
+    
+    
+    
     // MARK: UICollectionViewDataSource
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -52,17 +126,41 @@ class LuminariaCollectionViewController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return luminaria.count
+        return luminariasDic.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "celdaCollection", for: indexPath) as! LuminariaCollectionViewCell
         
+        
+        //Tratamiento de imagenes de producto
+        let imagenString = luminariasDic[indexPath.row]["FOTO"]!
+        if imagenString.isEmpty || imagenString == "" {
+            cell.imagenContenedor.image = UIImage(named: "imagenNoDisponible")
+            
+        } else {
+            // Cargar imagen desde una URL
+            // creamos un objeto url para introducir la URL
+            let urlACargar = URL(string: imagenString)
+            // cogemos el contenido de una URL y lo convertimos a un objeto Data
+            let data = try? Data(contentsOf: urlACargar!)
+            //Cargamos el objeto data en el UIImage
+            if (data == nil){
+                cell.imagenContenedor.image = UIImage(named: "imagenNoDisponible")
+            } else {
+                cell.imagenContenedor.image = UIImage(data: data!)
+            //redondea los bordes de la imagen
+            //cell.imagenContenedor.layer.cornerRadius = 30.0
+            //cell.imagenContenedor.clipsToBounds = true
+            }
+        
+        }
+        
         //Customizarla
-        cell.nombreLuminariaLabel.text = luminaria[indexPath.row].nombre
-        cell.lumenesLabel.text = String(luminaria[indexPath.row].lumenes)
-        cell.aperturaLabel.text = String(luminaria[indexPath.row].apertura)
-        cell.imagenContenedor.image = UIImage(named: "light-bulb" )
+        cell.nombreLuminariaLabel.text = luminariasDic[indexPath.row]["NOMBRE"]
+        cell.lumenesLabel.text = luminariasDic[indexPath.row]["LUMENES"]
+        cell.aperturaLabel.text = luminariasDic[indexPath.row]["APERTURA"]
+        //cell.imagenContenedor.image = UIImage(named: "light-bulb" )
         cell.imagenContenedor.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
         cell.imagenContenedor.layer.borderWidth = 2
         cell.imagenContenedor.layer.cornerRadius = 3
